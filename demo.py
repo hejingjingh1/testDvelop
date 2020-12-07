bd8e4449bb66fe0468c95e825d6819eccde09625
ghejingjingh1 <564352519@qq.com>
2020-12-07 20:08:59 +0800

    first commit

---
+++ demo.py
@@ -0,0 +1,7 @@
+print(2+2);
+print("""\
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+""");
+print('py''thon');
